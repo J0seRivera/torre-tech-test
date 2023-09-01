085c723c67fda0c85a6ea0932de8f086952ef381
Jose_Rivera <joseriveradl@gmail.com>
2023-08-31 23:01:14 -0600

    :art: Style: SearchBar component and syles
    changes in index.css adding variables and changing to relative measures for font size

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import "./SearchBar.css"
+import searchIcon from "../../assets/icons/search.png"
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <div className='searchBar'>
+      <span className='searchIcon'>
+        <img src={searchIcon} alt="search icon" />
+      </span>
+      <input className='searchInput' type="text" placeholder='Searh people by name' />
+    </div>
   )
 }
 
